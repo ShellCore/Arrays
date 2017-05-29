1b4384ba6dd7cf2a868066c2ab51dfed64ae7f53
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-05-29 11:00:24 -0500

    Costrucción de arreglos de objetos.

---
+++ src/com/shellcore/java/arrays/Foo.java
@@ -0,0 +1,17 @@
+package com.shellcore.java.arrays;
+
+/**
+ * Created by Cesar. 26/05/2017.
+ */
+public class Foo {
+    private int id;
+
+    public Foo(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Soy el objeto Foo #" + this.id;
+    }
+}
