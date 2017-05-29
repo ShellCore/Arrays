ae311881881577d183415e578b76e0e4403cf13b
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-05-29 16:00:44 -0500

    Se creó ejemplo en donde se esperan los elementos capturados por el usuario, y cuando se escribe la opción "EXIT", la aplicación termina de almacenar los elementos y muestra la lista con todo lo inicilializado y capturado.

---
+++ src/com/shellcore/java/arrays/CreencastArrayList.java
@@ -0,0 +1,31 @@
+package com.shellcore.java.arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by Cesar. 29/05/2017.
+ */
+public class CreencastArrayList {
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+
+        list.add("Banana");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input EXIT when you have finished.");
+
+        String item = "";
+
+        do {
+            item = scanner.next();
+
+            if (!item.equalsIgnoreCase("EXIT") && !list.contains(item)) {
+                list.add(item);
+            }
+        } while (!item.equalsIgnoreCase("EXIT"));
+
+        System.out.println(list);
+    }
+}
